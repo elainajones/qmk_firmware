62aaec20191c802e86f39488ca27872cf2b7f3e0
Elaina Jones <133198355+elainajones@users.noreply.github.com>
2025-12-23 13:13:37 -0800

    docs: Add comments for X value compensation
    Adds comments explaining how and why the X value compensation is
    applied to the lefy Ploopy Classic to counteract the clockwise bias
    to vertical cursor movement.

--- keyboards/ploopyco/trackball/keymaps/elainajones/keymap.c
+++ keyboards/ploopyco/trackball/keymaps/elainajones/keymap.c
@@ -18,39 +18,53 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT( /* Base */
-        KC_BTN1, KC_BTN3, KC_BTN3,
-          KC_BTN2, KC_BTN4
+    // Layout indexes correspond to physical buttons (left handed mouse).
+    // Values assigned to these indexes indicate button functions.
+    //
+    // BTN1 = primary click
+    // BTN2 = "right"/secondary click
+    // BTN3 = middle click
+    // BTN4 = extra (previous page)
+    // BTN5 = extra (next page)
+    [0] = LAYOUT(
+        KC_BTN1, // Physical right-side lower (primary click)
+        KC_BTN3, // Physical right-side upper (middle click)
+        KC_BTN3, // Physical right-side middle scroll wheel (middle click)
+        KC_BTN2, // Physical left-side upper ("right"/secondary click)
+        KC_BTN4  // Physical left-side lower (extra)
     ),
-    /* 1    Right lower
-     * 2    Right upper
-     * 3    Right middle
-     * 4    Left 
-     */
 };
-
-//#undef PRINTF_SUPPORT_DECIMAL_SPECIFIERS
-//#define PRINTF_SUPPORT_DECIMAL_SPECIFIERS 1
-//#include "print.h"
 
 float x_rem = 0.0;
 report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
-    const float cos_theta = 0.866025;
-    const float sin_theta = 0.500000;
-    
+    /* Correction to vertical (up and down) mouse movements which
+     * skew counterclockwise while horizontal (left and right) movements
+     * remain level. This is a typical "rotate a point about the origin"
+     * problem with the following corresponding formulas.
+     *
+     *     x1 = x0 * cos(theta) - y0 * sin(theta)
+     *     y1 = x0 * sin(theta) + y0 * cos(theta)
+     *
+     *  Since the adjustment is only needed for vertical movements, only
+     *  the x value is modified to compensate for being off axis. This
+     *  essentially makes the vertical and horizontal axis
+     *  non-perpendicular
+     */
+
+    // 30 degrees clockwise
+    const float cos_theta = 0.866025;  // cos(30)
+    const float sin_theta = 0.500000;  // sin(30)
+
+    // New X value after adjusting 30 degrees clockwise (with remaining value
+    // from previous report to compensate for int return type)
     float x_new = mouse_report.x * cos_theta - mouse_report.y * sin_theta + x_rem;
 
     mouse_report.x = x_new;
+    // mouse_report.x is an integer but the new X value is a float
+    // so record the difference to apply for next time. This smoothes
+    // out horizontal (y=0) movements preventing "sticking" from partial
+    // values being disregarded.
     x_rem = x_new - mouse_report.x;
 
     return mouse_report;
 }
-
-//void keyboard_post_init_user(void) {
-//  // Customise these values to desired behaviour
-//  debug_enable=true;
-//  debug_matrix=true;
-//  //debug_keyboard=true;
-//  debug_mouse=true;
-//}
-
